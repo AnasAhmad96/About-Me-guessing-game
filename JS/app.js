fb5107dcff24b615306d94aa01d6421a8dca7e93
Anas Ahmad <85110440+AnasAhmad96@users.noreply.github.com>
2021-06-23 00:09:23 +0300

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,55 +1,40 @@
-'use strict';
+//'use strict';
 
-let score = 0;
-alert("take a few second to guess who i'm ");
+//let score = 0;
+//alert("take a few second to guess who i'm ");
 
-let userName = prompt('What is your name?');
-alert('You Are Welcom ' + userName);
+//let userName = prompt('What is your name?');
+//alert('You Are Welcom ' + userName);
 
 
 
-let ans = prompt("Are you studied computer science ?");
-let ans = prompt("Are your stidied computer science  ?");
-
+//let ans = prompt("Are you studied computer science ?")
 
 switch (ans.toLowerCase()) {
     case 'yes':
     case 'y':
-
         alert('great');
         score++;
-        alert('wonderful')
-
         break;
-
     case 'no':
     case 'n':
-
         alert('you need more focus');
-        alert('be focus');
         break;
-
     default:
         alert('wrong answer');
 }
 
-
 let answer = prompt('your age 25 ?');
-
 switch (answer.toUpperCase()) {
-
     case 'YES':
     case 'Y':
         alert('you got alegal age ');
         score++;
         break;
-
     case 'NO':
     case 'N':
-        alert('you cant conitnue with us')
         alert('you are not allowd to enter')
         break;
-
     default:
         alert('You Enter wrong answer');
 }
@@ -57,58 +42,48 @@ switch (answer.toUpperCase()) {
 let answer1 = prompt('Are you Student at ASAC?');
 
 switch (answer1.toUpperCase()) {
-
     case 'YES':
     case 'Y':
         alert('Welcome');
         score++;
         break;
-
     case 'NO':
     case 'N':
         alert('Join Us Pleas');
         break;
-
     default:
         alert('You Enter wrong answer');
 }
 
 let answer2 = prompt('this is first time to creat website');
-
 switch (answer2.toLowerCase()) {
     case 'yes':
     case 'y':
         alert('Ok we will start');
         score++;
         break;
-
     case 'no':
     case 'n':
         alert('Hope you enjoy');
         break;
-
     default:
         alert('wrong answer');
 }
 
-let answer3 = prompt('we are read !')
-
+let answer3 = prompt('we are ready !')
 switch (answer3.toLowerCase()) {
     case 'yes':
     case 'y':
         alert('Lets go');
         score++;
         break;
-
     case 'no':
     case 'n':
-        alert('You Enter wrong answer');
+        alert('oops');
         break;
-
     default:
         alert('You Enter wrong answer');
 }
-
 /*add 6 question to let user guess and more about me*/
 alert('We will play and u have 4 attempt to guess what the true number');
 let UserAns = prompt('Guess Number between 1 to 10');
@@ -121,7 +96,6 @@ for (let i = 0; i < 3; i++) {
     else if (UserAns <= 2) {
         alert('its too low, guess the number again');
         UserAns = prompt('Guess Number between 1 to 10');
-
     }
     else if (UserAns >= 8) {
         alert('too high, guess the number again');
@@ -136,7 +110,6 @@ for (let i = 0; i < 3; i++) {
 alert('We will take qick quiz at math and you have 6 attempt if your answer is false');
 let testanswer = prompt('what is the result for this equation (10*2/4 = "4,2,5,10")');
 let Test = [4, 2, 5, 10];
-
 for (let index = 5; index >= 0; index--) {
     if (index == 5) {
         alert('Your answer is correct, you are great');
@@ -152,6 +125,5 @@ for (let index = 5; index >= 0; index--) {
     }
 }
 alert('Great work ' + userName + 'and your score is ' + score);
-alert('You Enter wrong answer');
 }
 
