35b161dc350d19ba8694f2ce0648f4865a6f95e7
aajaber <aajaber.97@gmail.com>
2021-06-23 14:22:26 +0300

    function were updated by the driver

--- JS/app.js
+++ JS/app.js
@@ -128,12 +128,13 @@ alert('Great work ' + userName + ' and your score is ' + score);
 
 
 
-/*=================This code was added by the Driver (Ahmad Jaber)===========Functions Lab04======*/
+/*=================This code was added by the Driver (Ahmad Jaber)===========Functions Lab04*/
 
 
 let userInput = prompt('Enter your name again : ');
-function reply(userInput){
-    return outPut='welcome ' +userName;
+
+function reply(userName){
+    return alert('welcome ' +userName);
 }
 reply(userInput);
 
